616251b0c70d2cefe0da11fc08092ea0beadd4db
washinson <sanya112001@yandex.ru>
2018-01-10 20:58:39 +0300

    продебажил эту дичь

--- treap.cpp
+++ treap.cpp
@@ -1,66 +1,69 @@
-struct Item
+struct Treap
 {
-	Item *l, *r;
-	int x, y;
-	Item(int _x, int _y) : x(_x), y(_y), l(0), r(0) {}
-};
-
-struct Dec
-{
-	Item* root = 0;
-
-	pair<Item*,Item*> split(Item* v, int x)
+	struct Item
 	{
-		if (v == 0)
+		int x, y, cnt;
+		Item *l, *r;
+		Item(int x, int y) : x(x), y(y), l(0), r(0), cnt(0) {}
+	};
+
+	Item* root = 0;
+	typedef pair<Item*, Item*> pII;
+
+	int cnt(Item* v)
+	{
+		return v ? v->cnt : 0;
+	}
+
+	void upd_cnt(Item*& v)
+	{
+		if (!v) return;
+		v->cnt = cnt(v->l) + cnt(v->r) + 1;
+	}
+
+	pII split(Item*& v, int x)
+	{
+		if (v == 0) return pII(0, 0);
+		else if (v->x < x)
 		{
-			return pair<Item*, Item*>(0, 0);
-		}
-		else if (x > v->x)
-		{
-			pair<Item*, Item*> p = split(v->r, x);
+			pII p = split(v->r, x);
 			v->r = p.first;
-			return pair<Item*, Item*>(v, p.second);
+			upd_cnt(v);
+			return pII(v, p.second);
 		}
 		else
 		{
-			pair<Item*, Item*> p = split(v->l, x);
+			pII p = split(v->l, x);
 			v->l = p.second;
-			return pair<Item*, Item*>(p.first, v);
+			upd_cnt(v);
+			return pII(p.first, v);
 		}
 	}
 
-	Item* merge(Item* l,Item* r)
+	Item* merge(Item*& l, Item*& r)
 	{
-		if (l == 0)
-		{
-			return r;
-		}
-		else if (r == 0)
-		{
-			return l;
-		}
-		else if (l->y > r->y)
+		if (l == 0) return r;
+		if (r == 0) return l;
+		if (l->y > r->y)
 		{
 			l->r = merge(l->r, r);
+			upd_cnt(l); 
 			return l;
 		}
 		else
 		{
 			r->l = merge(l, r->l);
+			upd_cnt(r);
 			return r;
 		}
 	}
 
 	void insert(Item*& v, Item* k)
 	{
-		if (v == 0)
+		if (v == 0) { v = k; }
+		else if (v->y < k->y)
 		{
-			v = k;
-			return;
-		}
-		else if (k->y > v->y)
-		{
-			pair<Item*, Item*> p = split(v, k->x);
+			pII p = split(v, k->x);
 			k->l = p.first; k->r = p.second;
 			v = k;
 		}
@@ -68,24 +71,32 @@ struct Dec
 		{
 			insert(v->x < k->x ? v->r : v->l, k);
 		}
+		upd_cnt(v);
 	}
 
-	void erase(Item*& v, int x)
+	void erase(Item*& v, int k)
 	{
 		if (v == 0) return;
-		if (v->x == x)
+		if (v->x == k)
 		{
-			Item* p = merge(v->l, v->r);
-			v = p;
-		}
-		else if (v->x > x)
-		{
-			erase(v->l, x);
+			v = merge(v->l, v->r);
 		}
 		else
 		{
-			erase(v->r, x);
+			erase(v->x < k ? v->r : v->l, k);
 		}
-		return;
+		upd_cnt(v);
 	}
 };
+
+typedef Treap::Item Item;
+
+set<int> tt;
+int getY()
+{
+	while (1)
+	{
+		int ch = rand() * rand();
+		if (tt.find(ch) == tt.end()) return tt.insert(ch), ch;
+	}
+}
