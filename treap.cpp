0073ef3d469d7ea2d4fd56dd371389890444c717
washinson <sanya112001@yandex.ru>
2018-01-23 15:49:31 +0300

    Update treap.cpp

--- treap.cpp
+++ treap.cpp
@@ -1,3 +1,109 @@
+struct treap
+{
+	struct Item
+	{
+		int y, x;
+		int sum, val;
+		Item *l, *r;
+		Item(int y, int val) : y(y), l(0), r(0), sum(val*val), val(val), x(1) {}
+	};
+
+	int get(Item*& v)
+	{
+		return v ? v->x : 0;
+	}
+
+	int getSum(Item*& v)
+	{
+		return v ? v->sum : 0;
+	}
+
+	void upd(Item*& v)
+	{
+		if (v == 0) return;
+		v->x = get(v->l) + get(v->r) + 1;
+		v->sum = getSum(v->l) + getSum(v->r) + v->val*v->val;
+	}
+
+	Item* root = 0;
+	typedef pair<Item*, Item*> pII;
+
+	Item* merge(Item*& l, Item*& r)
+	{
+		if (l == 0) return r;
+		if (r == 0) return l;
+
+		if (l->y > r->y)
+		{
+			Item* p = merge(l->r, r);
+			l->r = p;
+			upd(l);
+			return l;
+		}
+		else
+		{
+			Item* p = merge(l, r->l);
+			r->l = p;
+			upd(r);
+			return r;
+		}
+	}
+
+	pII split(Item*& v, int i)
+	{
+		if (v == 0)
+			return pII(0, 0);
+		else if (get(v->l) >= i)
+		{
+			pII p = split(v->l, i);
+			v->l = p.second;
+			upd(v);
+			return pII(p.first, v);
+		}
+		else
+		{
+			pII p = split(v->r, i - get(v->l) - 1);
+			v->r = p.first;
+			upd(v);
+			return pII(v, p.second);
+		}
+	}
+
+	void insert(Item*& v, int i, Item* it)
+	{
+		pII p = split(v, i);
+		p.first = merge(p.first, it);
+		root = merge(p.first, p.second);
+	}
+
+	void erase(Item*& v, int i)
+	{
+		pII p = split(v, i);
+		pII p2 = split(p.second, 1);
+		root = merge(p.first, p2.second);
+	}
+
+	int get(Item*& v, int i)
+	{
+		pII p = split(v, i);
+		pII p2 = split(p.second, 1);
+		int t = p2.first->val;
+		Item* w = merge(p2.first, p2.second);
+		root = merge(p.first, w);
+		return t;
+	}
+
+	void add(Item*& v, int i, int a)
+	{
+		pII p = split(v, i);
+		pII p2 = split(p.second, 1);
+		p2.first->val += a;
+		upd(p2.first);
+		Item* w = merge(p2.first, p2.second);
+		root = merge(p.first, w);
+	}
+};
+
 struct Treap
 {
 	struct Item
