bf6fe6551becf4e849a0ac50c9e5cfccf1217a6b
washinson <sanya112001@yandex.ru>
2018-03-29 18:43:38 +0300

    Update mainTemplate.cpp

--- mainTemplate.cpp
+++ mainTemplate.cpp
@@ -20,11 +20,12 @@ using namespace std;
 #define forn(i,n) for(int i = 0; i < n; i++)
 #define forr(i,n) for(int i = n-1; i >= 0; i++)
 #define ALL(x) x.begin(),x.end()
-#define mp(x,y) make_pair(x,y)
+#define mp make_pair
 #define sf(x,y) scanf("%" x,&y)
 #define pf(x,y) printf("%" x,y)
 
 typedef long long int64;
+typedef long double ld;
 #define int int64
 
 typedef unsigned long long uint64;
@@ -32,7 +33,7 @@ typedef pair<int, int> pii;
 typedef pair<int64, int64> pii64;
 typedef vector<int> vint;
 typedef vector<int64> vint64;
-typedef long double float64;
+
 template <typename T1, typename T2> istream& operator >> (istream& in, pair<T1, T2>& t) { return in >> t.first >> t.second; }
 template <typename T1, typename T2> ostream& operator << (ostream& out, pair<T1, T2>& t) { return out << t.first << " " << t.second << endl; }
 template <typename T> istream& operator >> (istream& in, vector<T>& t) { for (int i = 0; i < t.size(); i++) in >> t[i]; return in; }
@@ -40,10 +41,11 @@ template <typename T> ostream& operator << (ostream& out, vector<T>& t) { for (i
 
 signed main()
 {
-	int r = 1e5;
+	int rrrr = 1e5;
 #ifdef _SANYA
-	freopen("abc.txt", "r", stdin);
-	r = 150;
+	freopen("abc.in", "r", stdin);
+	freopen("abc.out", "w", stdout);
+	rrrr = 150;
 #else
 #define T "rings"
 	//freopen(T ".in", "r", stdin);
